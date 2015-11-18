197b5a19502a2b0d3cb3419a2e9ba2434cb73a46
MoonWatcher582 <erbronne@gmail.com>
2015-11-17 22:44:36 -0500

    removed newlines from hashed values

--- code/mongo/sentiments.rb
+++ code/mongo/sentiments.rb
@@ -12,10 +12,16 @@ pos_table = Set.new
 File.open(pos_file, 'r') do |file|
 	pos_table.merge file.each_line
 end
+pos_table.map! do |k,v|
+	k.chomp
+end
 
 neg_table = Set.new
 File.open(neg_file, 'r') do |file|
 	neg_table.merge file.each_line
+end
+neg_table.map! do |k,v|
+	k.chomp
 end
 
 responses = client[:unlabel_review].find.map do |review|
